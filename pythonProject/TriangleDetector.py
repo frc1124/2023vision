ef132f6f552bf035b4e1f3c12e7e085721cf9ed9
AbdulWasTaken <armohammed9162@gmail.com>
2023-02-16 14:40:16 -0500

    shape work fr fr

--- pythonProject/TriangleDetector.py
+++ pythonProject/TriangleDetector.py
@@ -1,17 +1,53 @@
 import cv2
 import numpy as np
 import math
-# Variables
 
+# Variables
 cone_width_reference = 90 #  90 pixel width at 6 ft
 height = 0
 x_pos = 0
-distance_from_cone = 0
+distance_from_robot = 10
+distance = 0
+current_obj = None
 
 # reading image
 vid = cv2.VideoCapture(0)
 #(hMin = 19 , sMin = 108, vMin = 71), (hMax = 30 , sMax = 255, vMax = 255)
 
+
+def convex_hull_pointing_up(ch):
+
+    points_above_center, points_below_center = [], []
+
+    x, y, w, h = cv2.boundingRect(ch)
+    aspect_ratio = w / h
+
+    if aspect_ratio < 0.8:
+        vertical_center = y + h / 2
+
+        for point in ch:
+            if point[0][
+                1] < vertical_center:
+                points_above_center.append(point)
+            elif point[0][1] >= vertical_center:
+                points_below_center.append(point)
+
+        left_x = points_below_center[0][0][0]
+        right_x = points_below_center[0][0][0]
+
+        for point in points_below_center:
+            if point[0][0] < left_x:
+                left_x = point[0][0]
+            if point[0][0] > right_x:
+                right_x = point[0][0]
+
+        for point in points_above_center:
+            if (point[0][0] < left_x) or (point[0][0] > right_x):
+                return False
+    else:
+        return False
+
+    return True
 
 while True:
     _, frame = vid.read()
@@ -19,8 +55,8 @@ while True:
     original = frame.copy()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lower_y = np.array([22, 108, 71], dtype="uint8") # 5, 10, 130
-    upper_y = np.array([30, 255, 255], dtype="uint8") # 80, 240, 24z
+    lower_y = np.array([22, 151, 71], dtype="uint8") # 5, 10, 130
+    upper_y = np.array([28, 255, 255], dtype="uint8") # 30, 240, 24z
     mask_y = cv2.inRange(frame, lower_y, upper_y)
 
     lower_p = np.array([117, 75, 75], dtype="uint8")  # 110, 141, 47
@@ -32,28 +68,47 @@ while True:
 
     cnts_y = cv2.findContours(mask_y, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts_y = cnts_y[0] if len(cnts_y) == 2 else cnts_y[1]
+
     cnts_p = cv2.findContours(mask_p, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts_p = cnts_p[0] if len(cnts_p) == 2 else cnts_p[1]
 
-    for c in cnts_y:
-        x, y, w, h = cv2.boundingRect(c)
-        if (w > 92.5):
+    if cnts_y != None:
+        for c in cnts_y:
+            x, y, w, h = cv2.boundingRect(c)
+            if (w > 50):
+                if (convex_hull_pointing_up(c)):
+                    distance = (635 / h) * 2
+                    current_obj = c
+                    distance_from_robot = distance
+                    x_pos = x
+
+    if (cnts_p != None):
+        for c in cnts_p:
+            x, y, w, h = cv2.boundingRect(c)
+            if (w > 62.5):
+                distance = (635 / h) * 2
+                if (distance_from_robot > distance):
+                    current_obj = c
+                    distance_from_robot = distance
+                    x_pos = x
+
+    if (cnts_y != None and cnts_p != None):
+        try:
+            x, y, w, h = cv2.boundingRect(current_obj)
             cv2.rectangle(original, (x, y), (x + w, y + h), (36, 255, 12), 2)
-            cv2.circle(original, (x+ int(w/2),y + int(h/2)), 20,  (36, 255, 12), 10)
-            height = h
-            x_pos = x
-
-    for c in cnts_p:
-        x, y, w, h = cv2.boundingRect(c)
-        if (w > 92.5):
-            cv2.rectangle(original, (x, y), (x + w, y + h), (36, 255, 12), 2)
-            cv2.circle(original, (x+ int(w/2),y + int(h/2)), 20,  (36, 255, 12), 10)
-
-
-    if (height != 0):
-        distance = (635 / height) * 2
+            cv2.circle(original, (x + int(w / 2), y + int(h / 2)), 20, (36, 255, 12), 2)
+        except:
+            print("L")
     else:
-        distance = 0
+        print(cnts_y)
+
+        x_pos = 0
+        distance_from_robot = 0
+        current_obj = None
+        original = ORIGv
+
+
+
     print(f"distance is {distance}", f" and Pixel height is {height}")
     print(f"Horizontal distance (px): {x_pos-360}")
 
